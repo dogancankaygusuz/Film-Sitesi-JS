60260751b42fab61d371bc1e3eda380aee2d4cc5
Doğancan Kaygusuz <142915576+dogancankaygusuz@users.noreply.github.com>
2026-03-19 15:19:07 +0300

    Add base path to Vite configuration

--- film-listesi/vite.config.ts
+++ film-listesi/vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins:[
+  base: './',
+  plugins: [
     react(),
     tailwindcss(),
   ],
